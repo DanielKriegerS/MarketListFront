ce5258cc39792063f7c4368e6409f0c8b596b84f
dschifel <danielschifelbain@gmail.com>
2025-03-21 18:21:48 -0300

    New: Implemented routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./components/market-list-create/market-list-create.component').then(
+            (m) => m.MarketListCreateComponent
+          ),
+        },                
+        {
+          path: 'all-lists',
+          loadComponent: () =>
+            import('./components/market-list-all/market-list-all.component').then(
+              (m) => m.MarketListAllComponent
+            ),
+        },
+        {
+          path: 'market-list/:id',
+          loadComponent: () =>
+            import('./components/market-list/market-list.component').then(
+              (m) => m.MarketListComponent
+            ),
+        },
+        {
+          path: 'finished-market-list-all',
+          loadComponent: () =>
+            import('./components/market-list-all/market-list-all.component').then(
+              (m) => m.MarketListAllComponent
+            ),
+        },
+        {
+          path: 'finished-market-list/:id',
+          loadComponent: () =>
+            import('./components/finished-market-list/finished-market-list.component').then(
+              (m) => m.FinishedMarketListComponent
+            ),
+        },
+        {
+          path: '**',
+          redirectTo: '', 
+          pathMatch: 'full',
+        },
+];
