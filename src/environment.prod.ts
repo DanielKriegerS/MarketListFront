7a9425baee82c6eb42d86560266ba4759a863769
dschifel <danielschifelbain@gmail.com>
2025-04-15 19:25:26 -0300

    Refactored: new api endpoint prefix

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://marketlistapi-production.up.railway.app'
+    apiUrl: 'https://marketlistapi-production.up.railway.app/api'
   };
   
