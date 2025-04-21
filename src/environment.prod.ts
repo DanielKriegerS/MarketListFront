c0d3fdef8741f350877d204eb42d642b277d07c7
dschifel <danielschifelbain@gmail.com>
2025-04-21 11:55:20 -0300

    Fixed: adjusted api url

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://marketlistapi-production.up.railway.app/api'
+    apiUrl: 'https://marketlistapi-production.up.railway.app/'
   };
   
