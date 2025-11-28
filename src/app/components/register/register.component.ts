b8e54bac320a5a9e480ca7d1c355d47ebef49802
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-28 12:21:19 -0300

    New: implemented auth service in register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthServiceService } from '../../services/auth-service.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+ username: string = '';
+ password: string = '';
 
+ constructor(private service : AuthServiceService) { }
+
+  register() {
+    if (!this.username.trim() || !this.password.trim()) {
+      alert('Por favor, preencha todos os campos.');
+      return;
+    }
+    this.service.register(this.username, this.password).subscribe({
+      next: (response) => {
+        alert(response.message);
+      },
+      error: (error) => {
+        alert('Erro ao registrar: ' + error.error.message);
+      }
+    });
+  }
 }
