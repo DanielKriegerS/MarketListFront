d2257d699b8cf973f30a9d46ec20aca8fe7fd338
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-28 12:19:37 -0300

    New: created authentication service

---
+++ src/app/services/auth-service.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  endpoint: string =  `${environment.apiUrl}/auth`;
+
+    constructor(private http: HttpClient) { }
+
+    login(username: string, password: string) {
+      return this.http.post<{accessToken: string}>(`${this.endpoint}/login`, {username, password})
+      .pipe(
+        tap(response => {
+          console.log('Storing token in localStorage:', response);
+          localStorage.setItem('authToken', response.accessToken);
+        })  
+      );
+    }
+
+    register(username: string, password: string) {
+      return this.http.post<{message: string}>(`${this.endpoint}/register`, {username, password});
+    }
+}
