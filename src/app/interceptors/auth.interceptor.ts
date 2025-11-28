3f7d75bdee266a9a774df64e7f93152834beb349
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-28 12:33:42 -0300

    Fix: removed log

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn } from "@angular/common/http";
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const token = localStorage.getItem('authToken');
-    console.log('Auth Interceptor - Retrieved token from localStorage:', token);
     
     if (token) {
         req = req.clone({
