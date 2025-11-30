12105ea52b7d491f3834729fdd4240984711848d
DanielKriegerS <danielschifelbain@gmail.com>
2025-11-30 18:01:22 -0300

    Feat: implemented logged / not logged views

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,32 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  islogged: boolean = false;
+
+  constructor() {
+    this.verifyLogin();
+  }
+
+  verifyLogin(): void {
+    const token = localStorage.getItem('authToken');
+    this.islogged = this.verifyToken(token);
+  }
+
+  verifyToken(token: string | null): boolean {
+    return token !== null && token.length > 0;
+  }
+
+  logout(): void {
+    localStorage.removeItem('authToken');
+    window.location.href = '/login';
+    this.islogged = false;
+  }
 }
